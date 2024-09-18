77a08ed8d72450016ecf50714dd093b36876281e
Gibson Murray <gibmurrays@gmail.com>
2024-09-18 18:15:16 -0400

    base commit for vue

---
+++ app.vue
@@ -0,0 +1,41 @@
+<script setup>
+  const description =
+    "My personal website - A portfolio of my best web development adventures";
+  const title = "Gibson Murray";
+
+  useSeoMeta({
+    title: title,
+    description: description,
+    ogTitle: title,
+    ogDescription: description,
+    ogImage: "/og-image.jpg",
+    ogUrl: "https://gibsonmurray.com",
+    twitterTitle: title,
+    twitterDescription: description,
+    twitterImage: "/twitter-image.jpg",
+    twitterCard: "summary",
+  });
+
+  useHead({
+    htmlAttrs: {
+      lang: "en",
+    },
+    link: [
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/favicon.ico",
+      },
+    ],
+  });
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+  body {
+    font-family: "Figtree", "Inter", sans-serif;
+  }
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/image", "@nuxt/fonts"],
+  fonts: {
+    families: [
+      {
+        name: "Figtree",
+        provider: "google",
+      },
+    ],
+  },
+});
