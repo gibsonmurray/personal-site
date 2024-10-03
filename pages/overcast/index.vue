846901117a33e8aae40d1d5c2f6119dabd31254d
Gibson Murray <gibmurrays@gmail.com>
2024-10-03 00:54:26 -0400

    bug fixes & changes

---
+++ pages/overcast/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="relative flex min-h-svh w-screen items-center justify-center overflow-y-auto bg-white py-10"
+    >
+        <ContentDoc path="/overcast" class="container prose" />
+    </div>
+</template>
